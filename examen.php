05122e13b782d017902d724e0caa89f630aa1a1e
MADV <madv@madv.es>
2024-10-31 09:07:14 +0000

    Segundo commit

--- examen.php
+++ examen.php
@@ -1,1 +1,13 @@
-<<?php echo 'Maria, 31/10/2024'?>
+<html>
+  <head>
+    <title>EXAMEN</title>
+  </head>
+
+  <body>
+    <h1>Maria</h1>
+    <h2>31/10/2024</h2>
+    <h3>Repositiorio</h3>
+  <<?php echo 'Maria, 31/10/2024'?>
+  </body>
+</html>   
+
